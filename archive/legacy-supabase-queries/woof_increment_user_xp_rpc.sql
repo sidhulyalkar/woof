6ce40206b27f55cbbe3a5d7317501ff365220b86
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2025-10-01 17:15:55 -0700

    Delete archive/legacy-supabase-queries directory
    Unneeded legacy code removed

--- archive/legacy-supabase-queries/woof_increment_user_xp_rpc.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE FUNCTION public.increment_user_xp(target_user UUID, delta INT)
-RETURNS VOID AS $$
-BEGIN
-  UPDATE public.users 
-  SET xp = GREATEST(0, xp + delta),
-      level = GREATEST(1, 1 + (xp + delta) / 100)
-  WHERE id = target_user;
-END; $$ LANGUAGE plpgsql SECURITY DEFINER;
