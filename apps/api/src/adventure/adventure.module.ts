224e3dbed3982d329e685091ca6a550335160768
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-21 15:37:41 -0700

    feat(adventure): register cooperative Pack challenges

--- apps/api/src/adventure/adventure.module.ts
+++ apps/api/src/adventure/adventure.module.ts
@@ -3,11 +3,13 @@ import { CareEventsModule } from '../care-events/care-events.module';
 import { InsightsModule } from '../insights/insights.module';
 import { AdventureController } from './adventure.controller';
 import { AdventureService } from './adventure.service';
+import { PackChallengesController } from './pack-challenges.controller';
+import { PackChallengesService } from './pack-challenges.service';
 
 @Module({
   imports: [InsightsModule, CareEventsModule],
-  controllers: [AdventureController],
-  providers: [AdventureService],
+  controllers: [AdventureController, PackChallengesController],
+  providers: [AdventureService, PackChallengesService],
   exports: [AdventureService],
 })
 export class AdventureModule {}
