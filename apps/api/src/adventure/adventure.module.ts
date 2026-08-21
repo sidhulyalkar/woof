a5af13577b2d3be584a40cc2f4874c2b66cb2ed4
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-21 16:29:09 -0700

    feat(adventure): register rollout guard

--- apps/api/src/adventure/adventure.module.ts
+++ apps/api/src/adventure/adventure.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CareEventsModule } from '../care-events/care-events.module';
 import { InsightsModule } from '../insights/insights.module';
+import { AdventureEnabledGuard } from './adventure-enabled.guard';
 import { AdventureController } from './adventure.controller';
 import { AdventureService } from './adventure.service';
 import { PackChallengesController } from './pack-challenges.controller';
@@ -9,7 +10,7 @@ import { PackChallengesService } from './pack-challenges.service';
 @Module({
   imports: [InsightsModule, CareEventsModule],
   controllers: [AdventureController, PackChallengesController],
-  providers: [AdventureService, PackChallengesService],
+  providers: [AdventureEnabledGuard, AdventureService, PackChallengesService],
   exports: [AdventureService],
 })
 export class AdventureModule {}
