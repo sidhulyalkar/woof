5a7f6eb5962332426f33c6cd5fc7736ec372afce
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-21 02:06:53 -0700

    feat(media): register derivative worker

--- apps/api/src/media-library/media-library.module.ts
+++ apps/api/src/media-library/media-library.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
 import { StorageModule } from '../storage/storage.module';
+import { MediaDerivativeWorkerService } from './media-derivative.worker';
 import { MediaLibraryController } from './media-library.controller';
 import { MediaLibraryService } from './media-library.service';
 
 @Module({
   imports: [PrismaModule, StorageModule],
   controllers: [MediaLibraryController],
-  providers: [MediaLibraryService],
+  providers: [MediaLibraryService, MediaDerivativeWorkerService],
   exports: [MediaLibraryService],
 })
 export class MediaLibraryModule {}
