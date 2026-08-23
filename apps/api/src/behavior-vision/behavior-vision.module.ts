e84a1bb7ab1032eb7a21dd839ae86ac14024e951
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-08-23 14:29:57 -0700

    Wire Behavior Moments shadow service

--- apps/api/src/behavior-vision/behavior-vision.module.ts
+++ apps/api/src/behavior-vision/behavior-vision.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BehaviorShadowService } from './behavior-shadow.service';
 import { BehaviorVisionController } from './behavior-vision.controller';
 import { BehaviorVisionModelService } from './behavior-vision.model';
 import { BehaviorVisionService } from './behavior-vision.service';
 
 @Module({
   controllers: [BehaviorVisionController],
-  providers: [BehaviorVisionService, BehaviorVisionModelService],
-  exports: [BehaviorVisionService],
+  providers: [BehaviorVisionService, BehaviorVisionModelService, BehaviorShadowService],
+  exports: [BehaviorVisionService, BehaviorShadowService],
 })
 export class BehaviorVisionModule {}
