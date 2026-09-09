616756a81edb2f0f7a2eeed9fbf49038295de69c
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-09 11:13:06 -0700

    Export shared Pack access authority

--- apps/api/src/social-adventure/social-adventure.module.ts
+++ apps/api/src/social-adventure/social-adventure.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { PackAccessService } from './pack-access.service';
 import { SocialAdventureShareCandidatesController } from './social-adventure-share-candidates.controller';
 import { SocialAdventureShareCandidatesService } from './social-adventure-share-candidates.service';
 import { SocialAdventureController } from './social-adventure.controller';
@@ -6,7 +7,7 @@ import { SocialAdventureService } from './social-adventure.service';
 
 @Module({
   controllers: [SocialAdventureController, SocialAdventureShareCandidatesController],
-  providers: [SocialAdventureService, SocialAdventureShareCandidatesService],
-  exports: [SocialAdventureService],
+  providers: [PackAccessService, SocialAdventureService, SocialAdventureShareCandidatesService],
+  exports: [PackAccessService, SocialAdventureService],
 })
 export class SocialAdventureModule {}
