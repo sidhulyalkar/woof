2ab1be1a31f0b04b61a3c53758efed042522f581
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-10 09:51:03 -0700

    Register Expedition field journal service

--- apps/api/src/expeditions/expeditions.module.ts
+++ apps/api/src/expeditions/expeditions.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { SocialAdventureModule } from '../social-adventure/social-adventure.module';
+import { ExpeditionJournalService } from './expedition-journal.service';
 import { ExpeditionsController } from './expeditions.controller';
 import { ExpeditionsService } from './expeditions.service';
 
 @Module({
   imports: [SocialAdventureModule],
   controllers: [ExpeditionsController],
-  providers: [ExpeditionsService],
+  providers: [ExpeditionsService, ExpeditionJournalService],
   exports: [ExpeditionsService],
 })
 export class ExpeditionsModule {}
