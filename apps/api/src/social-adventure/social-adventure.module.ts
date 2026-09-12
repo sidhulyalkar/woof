c64ee1f0950c6b85140c7d401ea733ad87a34a88
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2026-09-11 23:29:27 -0700

    Register Pack locality authority

--- apps/api/src/social-adventure/social-adventure.module.ts
+++ apps/api/src/social-adventure/social-adventure.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PackAccessService } from './pack-access.service';
+import { PackLocalityService } from './pack-locality.service';
 import { SocialAdventureShareCandidatesController } from './social-adventure-share-candidates.controller';
 import { SocialAdventureShareCandidatesService } from './social-adventure-share-candidates.service';
 import { SocialAdventureController } from './social-adventure.controller';
@@ -7,7 +8,12 @@ import { SocialAdventureService } from './social-adventure.service';
 
 @Module({
   controllers: [SocialAdventureController, SocialAdventureShareCandidatesController],
-  providers: [PackAccessService, SocialAdventureService, SocialAdventureShareCandidatesService],
-  exports: [PackAccessService, SocialAdventureService],
+  providers: [
+    PackAccessService,
+    PackLocalityService,
+    SocialAdventureService,
+    SocialAdventureShareCandidatesService,
+  ],
+  exports: [PackAccessService, PackLocalityService, SocialAdventureService],
 })
 export class SocialAdventureModule {}
